6ab87b972b3804635ad0fdb661d727d17c50fda4
phuonghq <phuongmt13kttn@gmail.com>
2018-01-04 17:34:18 +0700

    #4/1/2018

--- train.py
+++ train.py
@@ -1,96 +1,229 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  1 14:51:13 2017
-
-@author: phuonghq
-"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import shutil
 import argparse
-import sys
-import tempfile
-import pandas as pd
-from six.moves import urllib
-import csv
+from tensorflow.contrib.learn.python.learn.estimators import constants
+from tensorflow.contrib.learn.python.learn.estimators.dynamic_rnn_estimator import PredictionType
+
+from math import sqrt
+from utils import *
 import numpy as np
 import tensorflow as tf
 import os
-from utils import *
+import shutil
+from tensorflow.python.estimator.export import export
 
 FLAGS = None
-DEFAULT_BATCH_SIZE = 4000
 
-EXPORT_DIR_BASE = rootPath + '/tmp/phuonghq/'
-tf.logging.set_verbosity(tf.logging.INFO)
+# tf.logging.set_verbosity(tf.logging.INFO)
 
 # Learning rate for the model
-# 0.0001 -0.0002
-LEARNING_RATE = 0.000001
+# LEARNING_RATE = 0.015  # 0.015
+EXPORT_DIR_BASE = rootPath + '/Model/phuonghq/'
+STEP_SIZE = 2000  # 7000
+VO_THI_SAU_1 = list(range(46431, 46451))
+VO_THI_SAU_1.extend(range(113547, 113557))
+# VO_THI_SAU_2 = list(range(113547, 113556))
+
+DUONG_3THANG2_1 = list(range(122000, 122080))
+DUONG_3THANG2_1.extend(range(122149, 122183))
+# DUONG_3THANG2_2 = list(range(122149, 122182))
+DUONG_3THANG2_3 = list(range(113270, 113292))
+
+TRUONG_CHINH_1 = list(range(101458, 101510))
+TRUONG_CHINH_2 = list(set(range(121213, 121240)) - set([121219]))
+
+CMT8_1 = list(range(117041, 117107))
+CMT8_2 = list(range(120628, 120641))
+CMT8_3 = list(range(114117, 114122))
+
+XVNT_1 = list(range(121063, 121087))
+XVNT_2 = list(range(2537, 2555))
+
+DBP = list(range(88084, 88164))
 
 
-def serving_input_receiver_fn():
-    """Build the serving inputs."""
-    inputs = {"x": tf.placeholder(shape=[None, 4], dtype=tf.float32)}
-    return tf.estimator.export.ServingInputReceiver(inputs, inputs)
+def train(frameRequest, offset, learning_rate):
+    # remove all file old model
+    if os.path.exists(EXPORT_DIR_BASE):
+        shutil.rmtree(EXPORT_DIR_BASE, ignore_errors=True)
+    # Load datasets
+    rootPath = os.path.abspath(os.path.dirname(__file__))
 
-def main(unused_argv):
-    #remove all file old model
-    if os.path.exists(EXPORT_DIR_BASE) == True:
-        shutil.rmtree(EXPORT_DIR_BASE)
-
-
-    # Training examples
-    training_set = load_data(rootPath,'/Data_train/','training_set')
-    print(training_set)
+    training_set = load_data(rootPath, '/Train/', 'training_set', frameRequest + 1, offset, 0)
+    print("train: ", training_set)
     # Test examples
-    test_set = load_data(rootPath,'/Data_test/','test_set')
-    print(test_set)
-    # Set model params
-    model_params = {"learning_rate": LEARNING_RATE}
-
-    # Instantiate Estimator
-    run_config = tf.contrib.learn.RunConfig()
-    run_config = run_config.replace(model_dir=EXPORT_DIR_BASE)
-    nn = tf.estimator.Estimator(model_fn=model_fn, params=model_params, config=run_config)
-
-# scale data
+    test_set = load_data(rootPath, '/Test/', 'test_set', frameRequest + 1, offset, 0)
+    print("test: ", test_set)
     scale_data(training_set.data)
     scale_data(test_set.data)
 
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x": training_set.data},
-        y=np.array(training_set.target),
-        num_epochs=None,  # 0
-        batch_size=DEFAULT_BATCH_SIZE,
-        shuffle=True)
+    # Set model params
+    # model_params = {"learning_rate": LEARNING_RATE}
+    xc = tf.contrib.layers.real_valued_column("x")
 
+    run_config = tf.estimator.RunConfig()
+    run_config = run_config.replace(
+        save_summary_steps=500,
+        model_dir=EXPORT_DIR_BASE,
+        save_checkpoints_steps=1000,
+        save_checkpoints_secs=None,
+        session_config=None,
+        keep_checkpoint_max=25,
+        keep_checkpoint_every_n_hours=10000,
+        log_step_count_steps=200
+    )
+
+    nn = tf.contrib.learn.DynamicRnnEstimator(problem_type=constants.ProblemType.LINEAR_REGRESSION,
+                                              prediction_type=PredictionType.SINGLE_VALUE,
+                                              sequence_feature_columns=[xc],
+                                              context_feature_columns=None,
+                                              num_units=10,
+                                              cell_type='lstm',
+                                              optimizer='SGD',
+                                              learning_rate=learning_rate,
+                                              # dropout_keep_probabilities = [0.5,0.5],
+                                              config=run_config
+                                              )  # 0.05
+    validation_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+        x={"x": np.array(test_set.data)},
+        y=np.array(test_set.target),
+        eval_steps=1
+        # every_n_steps=10,
+        # early_stopping_metric="loss",
+        # early_stopping_metric_minimize=True,
+        # early_stopping_rounds=10
+    )
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": np.array(training_set.data)},
+        y=np.array(training_set.target),
+        num_epochs=None,
+        batch_size=50,
+        shuffle=True)
+    print("train input fn: ", train_input_fn)
     # Train
-    nn.train(input_fn=train_input_fn, steps=50000)
-    # export
-    nn.export_savedmodel(EXPORT_DIR_BASE,
-        serving_input_receiver_fn=serving_input_receiver_fn)
+    nn.fit(input_fn=train_input_fn, steps=STEP_SIZE, monitors=[validation_monitor])
 
     # Score accuracy
     test_input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"x":test_set.data},
+        x={"x": np.array(test_set.data)},
         y=np.array(test_set.target),
+        batch_size=50,
         num_epochs=1,
-        batch_size=DEFAULT_BATCH_SIZE,
         shuffle=False)
 
     ev = nn.evaluate(input_fn=test_input_fn)
-    print("Loss: %s" % ev["loss"])
-    print("Root Mean Squared Error: %s" % ev["rmse"])
-    f = open(EXPORT_DIR_BASE + 'ev.csv', 'w')
-    f.write('MAE: ' + str(ev['mae']) + ',' + 'RMSE: ' + str(
-                                ev['rmse']) + ',' + 'MAPE: ' + str(ev['mape']) + ',' + 'MASE: ' + str(ev['mase']) )
-    f.close()
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.register("type", "bool", lambda v: v.lower() == "true")
-    FLAGS, unparsed = parser.parse_known_args()
-    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
 
+    # print("Root Mean Squared Error: %s" % ev["rmse"])
+    prediction_set, segment_id = load_data(rootPath, '/Predict/', 'prediction_set', frameRequest + 1, int(offset),
+                                           1)  # dataset more 1 column with segment
+
+    # print(prediction_set.data[:5])
+    scale_data(prediction_set.data)
+    predict_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": np.array(prediction_set.data)},
+        num_epochs=1,
+        shuffle=False)
+
+    predictions = nn.predict(input_fn=predict_input_fn)
+
+    output = []
+    for i, p in enumerate(predictions):
+        output.append(p["scores"])
+        if i < 2:
+            print("Prediction %s: %s" % (i + 1, p))
+
+    print(prediction_set.target[1:5])
+    print(output[1:5])
+
+    # f = open(EXPORT_DIR_BASE + 'ev.csv', 'w')
+    # f.write('MAE: ' + str(mae) + ',' + 'RMSE: ' + str(
+    #     rmse) + ',' + 'MAPE: ' + str(mape) + ',' + 'MASE: ' + str(mase))
+    # f.close()
+    file_plot, df_out = print_file(rootPath, output, frameRequest)
+    print(file_plot)
+    # dictLinePlot = {'name': linePlotName,'url':linePlotUrl}
+
+    # velo, count_seg = get_velocity(VO_THI_SAU_1, file_plot, frameRequest)
+    return output
+
+
+def main(frameRequest, offset, learning_rate):
+    day_DF = pd.read_csv(rootPath + '/Predict/Predict/tmp_predict.csv', header=None,
+                         usecols=[0])
+
+    for i in range(0, 96):
+        output = train(i, offset, learning_rate)
+        output_dayDF = pd.DataFrame(output)
+        day_DF = pd.concat([day_DF, output_dayDF], axis=1)
+        print(day_DF)
+
+    day_DF.to_csv(rootPath + '/Temp/' + 'final_data.csv', sep=',', index=False, header=None)
+
+
+def predict(frameRequest):
+    output_file = rootPath + '/Temp/' + 'final_data.csv'
+    df_predict = pd.read_csv(output_file, header=None,
+                             usecols=[frameRequest + 1])
+    real_file = rootPath + '/Predict/Predict/tmp_predict.csv'
+    df_real = pd.read_csv(real_file, header=None,
+                          usecols=[0, frameRequest + 1])
+
+    delta_valueDF = abs(df_real[frameRequest + 1] - df_predict[frameRequest + 1]) / df_real[frameRequest + 1] * 100
+    delta_valueDF = pd.DataFrame(delta_valueDF)
+
+    # segDF = pd.DataFrame(df_real[0])
+
+    rmse = sqrt(mean_squared_error(np.array(df_real[frameRequest + 1]), np.array(df_predict[frameRequest + 1])))
+    mae = mean_absolute_error(np.array(df_real[frameRequest + 1]), np.array(df_predict[frameRequest + 1]))
+
+    mape = mean_absolute_percentage_error(np.array(df_real[frameRequest + 1]), np.array(df_predict[frameRequest + 1]))
+    mase = mean_absolute_scaled_error(np.array(df_real[frameRequest + 1]), np.array(df_predict[frameRequest + 1]))
+
+    print("Root Mean Squared Error: %s" % rmse)
+    print("Mean Absolutely Error: %s" % mae)
+    print("Mean Absolutely Percentage Error: %s" % mape)
+    print("Mean Absolutely Scaled Error: %s" % mase)
+    data_frame = pd.concat([df_real, df_predict, delta_valueDF], axis=1)
+    dictCMT8 = street_plot(CMT8_1, output_file, frameRequest, 'CMT8', 'Cach Mang Thang 8', False)
+    dictVTS = street_plot(VO_THI_SAU_1, output_file, frameRequest, 'VOTHISAU', 'Vo Thi Sau', False)
+    dict3THANG2 = street_plot(DUONG_3THANG2_1, output_file, frameRequest, '3_THANG_2', '3 Thang 2', False)
+    dictDBP = street_plot(DBP, output_file, frameRequest, 'DBP', 'Dien Bien Phu', False)
+    dictPlot = {'CMT8': dictCMT8, 'VTS': dictVTS, '3_THANG_2': dict3THANG2, 'DBP': dictDBP}
+
+    # ARIMA
+    ARIMA_dir = rootPath + '/ARIMA/Result/'
+    ARIMAError_dir = rootPath + '/ARIMA/Evaluate/'
+    arima_plot = ARIMA_dir + 'Frame_' + str(frameRequest) + '.csv'
+    arima_CMT8 = street_plot(CMT8_1, arima_plot, frameRequest, 'CMT8', 'Cach Mang Thang 8', True)
+    arima_VTS = street_plot(VO_THI_SAU_1, arima_plot, frameRequest, 'VOTHISAU', 'Vo Thi Sau', True)
+    arima_3THANG2 = street_plot(DUONG_3THANG2_1, arima_plot, frameRequest, '3_THANG_2', '3 Thang 2', True)
+    arima_DBP = street_plot(DBP, arima_plot, frameRequest, 'DBP', 'Dien Bien Phu', True)
+    arima_Dict_Plot = {'CMT8': arima_CMT8, 'VTS': arima_VTS, '3_THANG_2': arima_3THANG2, 'DBP': arima_DBP}
+
+    ARIMA_df = pd.read_csv(ARIMA_dir + 'Frame_' + str(frameRequest) + '.csv', usecols=[0, frameRequest, 97, 98],
+                           header=None)
+
+    ARIMA_error = pd.read_csv(ARIMAError_dir + 'Frame_' + str(frameRequest) + '_ev' + '.csv', header=None, skiprows=[1])
+    ARIMA_error = ARIMA_error.values
+
+    return data_frame, ARIMA_df, dictPlot, arima_Dict_Plot, output_file, rmse, mae, mape, mase, ARIMA_error[0][1], \
+           ARIMA_error[0][0], ARIMA_error[0][2], ARIMA_error[0][3]
+
+
+if __name__ == "__main__":
+    with tf.device('/gpu:0'):
+        parser = argparse.ArgumentParser()
+        parser.register("type", "bool", lambda v: v.lower() == "true")
+        FLAGS, unparsed = parser.parse_known_args()
+        parser.add_argument('--frame', metavar='path', required=True,
+                            help='the path to frame')
+        parser.add_argument('--history', metavar='path', required=True,
+                            help='path to history')
+        parser.add_argument('--learning_rate', metavar='path', required=True,
+                            help='path to learning_rate')
+        args = parser.parse_args()
+
+        main(frameRequest=int(args.frame), offset=int(args.history), learning_rate=float(args.learning_rate))
+        # tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
--- utils.py
+++ utils.py
@@ -30,15 +30,17 @@ IMG_DIR = rootPath + '/Output'
 # OFFSET = 10
 MAX_FRAME = 97
 
-def street_plot(name_street,file_plot,frameRequest,name,description,isArima):
+
+def street_plot(name_street, file_plot, frameRequest, name, description, isArima):
     velo, count_seg = get_velocity(name_street, file_plot, frameRequest)
-    #name_street+'_name', name_street+'_url' = plot_by_street(name_street, velo, name, count_seg, frameRequest, description)
-    name,url= plot_by_street(name_street, velo, name, count_seg, frameRequest, description,isArima)
-    print(name,url)
-    return {'name': str(name) , 'url': str(url) }
-   
+    # name_street+'_name', name_street+'_url' = plot_by_street(name_street, velo, name, count_seg, frameRequest, description)
+    name, url = plot_by_street(name_street, velo, name, count_seg, frameRequest, description, isArima)
+    print(name, url)
+    return {'name': str(name), 'url': str(url)}
+
+
 def load_export_model_dir(frRequest):
-    for subDir in os.listdir(TEMP_DIR +str(frRequest)):
+    for subDir in os.listdir(TEMP_DIR + str(frRequest)):
         if subDir is None:
             raise ValueError('Model is not exist!')
         else:
@@ -68,20 +70,25 @@ def load_data(rootPath, dir, dataset, frRequest, offset, flag):
         filePath = sub_dir + "/" + filePath
     print(filePath)
 
-    df = pd.read_csv(filePath, header=None, index_col=None,
-                     usecols=use_cols, low_memory=False)
-
-    df = df[use_cols]
+    df = pd.read_csv(filePath, header=None,
+                     usecols=use_cols)
+    print(df)
+    print("__________")
+    # df = df[use_cols]
 
     if flag == 1:
         seg_df = pd.read_csv(filePath, header=None,
-                             usecols=[0], low_memory=False)
+                             usecols=[0])
         segment_id = np.array(seg_df[0])
+    print('DDDDD')
+    print(df)
 
-    list_.append(df)
-    frame = pd.concat(list_)
-
-    frame.to_csv(rootPath + '/Temp/' + dataset + '.csv', sep=',', index=False)
+    columns = df.columns.tolist()
+    index_i = columns.index(frRequest)
+    columns = columns[(index_i + 1):] + columns[:(index_i + 1)]
+    df = df[columns]
+    print(df)
+    df.to_csv(rootPath + '/Temp/' + dataset + '.csv', sep=',', index=False, header=None)
     dataset = tf.contrib.learn.datasets.base.load_csv_without_header(
         filename=rootPath + '/Temp/' + dataset + '.csv', target_dtype=np.float32, features_dtype=np.float32)
     if flag == 1:
@@ -109,31 +116,24 @@ def print_file(rootPath, predict, frRequest):
                 if file is not None:
                     filePath = sub_dir + "/" + file
                     print(filePath)
-                    df = pd.read_csv(filePath, header=None, index_col=0, low_memory=False)
+                    df = pd.read_csv(filePath, header=None)
                     length = df.shape[0]
 
                     count_col = len(df.columns)
-                    predictArr = predictArr[1:]
                     df[count_col + 1] = predictArr
-                    print(df[count_col + 1])
-                    
+
                     # print(frRequest)
                     # print(df[count_col + 1])
                     #  print(df[frRequest])
-                    deltaArr = abs(df[count_col + 1] - df[frRequest]) / df[frRequest] * 100
-                    df[count_col + 2] = deltaArr
-                    print(df[count_col + 2])
-                   
-                    df.to_csv(rootPath + '/Temp/' + 'test_' + file, sep=',')
+                    # deltaArr = abs(df[count_col + 1] - df[frRequest]) / df[frRequest] * 100
+                    # df[count_col + 2] = deltaArr
+                    # print(df[count_col + 2])
+
+                    df.to_csv(rootPath + '/Temp/' + 'test_' + file, sep=',', index=False, header=None)
+
     file_plot = rootPath + '/Temp/' + 'test_' + file  ###
-    
+
     return file_plot, df
-
-
-def get_segment(file_plot):
-    df = pd.read_csv(file_plot, index_col=0, usecols=[0, 2])
-    c = df['Segment Id'].value_counts()
-    return c.index[1]
 
 
 def mean_absolute_percentage_error(y_true, y_pred):
@@ -153,91 +153,82 @@ def mean_absolute_scaled_error(y_true, y_pred):
     return np.mean(np.abs(errors / np.mean(np.abs(d))))
 
 
-def newPlot(veloToMap, seg_plot):
+def newPlot(predictDF, realDF, ARIMADF, seg_plot):
     fig = plt.figure()
+    fig.set_size_inches(12, 7, forward=True)
     fig.suptitle('Velocity', fontsize=14, fontweight='bold')
 
     ax = fig.add_subplot(111)
-    fig.subplots_adjust(top=0.85)
+    fig.subplots_adjust(left=0.1, right=0.9)
 
     ax.set_title('Segment: ' + str(seg_plot))
     ax.set_xlabel('Frame')
     ax.set_ylabel('Velocity')
-    ax.tick_params(axis='y', which='major', pad=15)
+    # ax.tick_params(axis='y', which='major', pad=15)
 
-    tmp = veloToMap
-    x = list(range(0, np.array(len(veloToMap))))
-    real = tmp[:-1]
-    real = real + [real[-1]]
-    # real.extend([tmp[-2]])
+    x = list(range(0, 96))
 
-    predict = tmp[:-2]
-    # print(predict)
-    predict = [predict[0]] + predict
-    predict.extend([tmp[-1]])
-
-    real = np.array([i for i in real])
-
-    predict = np.array([i for i in predict])
-    # plt.xticks(np.arange(min(x), max(x)+1, 5.0))
-    plt.yticks(np.arange(min(real), max(real) + 1, 5.0))
-    plt.plot(x, predict)
-    plt.plot(x, real)
-    plt.legend(['Predict', 'Real'], loc='upper left')
+    # plt.yticks(np.arange(min(realDF.values), max(real) + 1, 5.0))
+    plt.plot(x, predictDF.values)
+    plt.plot(x, realDF.values)
+    plt.plot(x, ARIMADF.values)
+    plt.legend(['Predict', 'Real', 'ARIMA'], bbox_to_anchor=(1, 1), loc=2, borderaxespad=0.)
     # plt.show()
     # print(fig)
     name = 'linePlot'
     url = rootPath + '/Output/Images/' + 'linePlot.png'
     fig.savefig(url)
+    fig.close()
     return name, url
 
 
-def linePlotBySeg(file_plot, frameRequest, seg_plot):
-    veloToMap = []
-
+def linePlotBySeg(file_plot, seg_plot):
+    predictVeloToMap = []
+    realVeloToMap = []
+    ARIMAVeloToMap = []
+    real_value_file = rootPath + '/Predict/Predict/tmp_predict.csv'
+    ARIMA_file = rootPath + '/Temp/final_data_ARIMA.csv'
     # funtion to get segment
-
-    # append velo
-    with open(file_plot, 'r') as f:
-        reader = csv.DictReader(f)
-        for row in reader:  # for each row in the reader object
-            seg_id = Decimal(row['0'])
-            if seg_id == seg_plot:
-                row = {int(k): float(v) for k, v in row.items()}
-                veloToMap = row
-    tmp = veloToMap.values()
-    # print(tmp,frameRequest)
-    velo = tmp[1:frameRequest + 1]
-
-    velo.append(str(tmp[-2]))
-    return newPlot(velo, seg_plot)
+    predict_DF = pd.read_csv(file_plot, header=None,
+                             index_col=[0])
+    real_DF = pd.read_csv(real_value_file, header=None,
+                          index_col=[0])
+    ARIMA_DF = pd.read_csv(ARIMA_file, header=None,
+                           index_col=[0])
+    predictVeloToMap = predict_DF.loc[seg_plot]
+    realVeloToMap = real_DF.loc[seg_plot]
+    ARIMAVeloToMap = ARIMA_DF.loc[seg_plot]
+    return newPlot(predictVeloToMap, realVeloToMap, ARIMAVeloToMap, seg_plot)
 
 
 def get_velocity(arrSeg, file_plot, frameRequest):
     veloToMap = []
-    with open(file_plot, 'r') as f:
-        reader = csv.DictReader(f)
-        for row in reader:  # for each row in the reader object
-            #print(row)
-            seg_id = int(float(row['0']))
-            if seg_id in arrSeg:
-                row = {int(k): float(v) for k, v in row.items()}
-                tmp = row
-                val = tmp.values()
-                temp = val[1: frameRequest + 1]
-                temp.append(val[-2])
-                veloToMap.append(np.array(temp))
+    segment = []
+    df = pd.read_csv(file_plot, header=None,
+                     index_col=[0])
+
+    for row in df.iterrows():
+        seg_id = int(float(row[0]))
+        if seg_id in arrSeg:
+            segment.append(seg_id)
+
+    for row in df.iterrows():
+        val = int(float(row[0]))
+        if val in segment:
+            tmp = row[1:]
+            # tmp.append
+            veloToMap.append(tmp)
 
     veloToMap = np.array(veloToMap)
-    n_segment = len(veloToMap)
+    n_segment = len(segment)
+
     veloToMap = veloToMap.ravel()
-    # print(len(veloToMap))
-    # print(veloToMap.shape)
+
     return veloToMap, n_segment
     # plot_by_street(arrAfterSeg,arrFr,arrVelo,name_street)
 
 
-def init_plot_by_street(x, val, location, delta, n_segment, frRequest, name,isArima):
+def init_plot_by_street(x, val, location, delta, n_segment, frRequest, name, isArima):
     y = list(range(0, len(x)))
     # print(x)
     # print(val)
@@ -251,7 +242,7 @@ def init_plot_by_street(x, val, location, delta, n_segment, frRequest, name,isAr
     ax.set_xlabel('Segment')
     ax.set_ylabel('Frame')
 
-    # get count for línpace
+    # get count for lÃ­npace
 
     # DELTA = 113547 - 46450
     # x_ = []
@@ -264,7 +255,7 @@ def init_plot_by_street(x, val, location, delta, n_segment, frRequest, name,isAr
 
 
 
-    zi = np.reshape(val, (-1, frRequest + 1))
+    zi = np.reshape(val, (n_segment, int(len(val) / n_segment)))  # must be 2D array
     # print(zi)
     zi = np.ma.masked_where(zi <= 0.00, zi)
 
@@ -272,7 +263,7 @@ def init_plot_by_street(x, val, location, delta, n_segment, frRequest, name,isAr
     cmap.set_bad(color='white')
     # print(zi)
     plt.imshow(zi, vmin=0, vmax=60, origin='lower',
-               extent=[0, n_segment, 0, frRequest], aspect="auto", cmap='jet_r', interpolation='None')
+               extent=[0, n_segment, 0, 96], aspect="auto", cmap='jet_r', interpolation='None')
     plt.subplots_adjust(hspace=0.5)
     plt.colorbar()
     # plt.show()
@@ -286,11 +277,11 @@ def init_plot_by_street(x, val, location, delta, n_segment, frRequest, name,isAr
     if isArima == False:
         url = rootPath + '/Output/Images/' + location + '.png'
     else:
-        url = rootPath + '/ARIMA/Images/' + location + '.png' 
+        url = rootPath + '/ARIMA/Images/' + location + '.png'
     fig.savefig(url)
     return name, url
 
 
-def plot_by_street(x, val, location, n_segment, frRequest, name,isArima):
+def plot_by_street(x, val, location, n_segment, frRequest, name, isArima):
     delta = max(abs(i - j) for (i, j) in zip(x[1:], x[:-1]))
-    return init_plot_by_street(x, val, location, delta - 1, n_segment, frRequest, name,isArima)
+    return init_plot_by_street(x, val, location, delta - 1, n_segment, frRequest, name, isArima)
